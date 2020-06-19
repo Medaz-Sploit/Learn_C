b9f02188749d529143cabe6a47ca265844ead549
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2020-06-19 18:34:30 +0100

    Learning

--- troisieme.c
+++ troisieme.c
@@ -6,7 +6,7 @@
 /*   By: mazoukni <mazoukni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 01:50:26 by mazoukni          #+#    #+#             */
-/*   Updated: 2020/06/19 04:26:41 by mazoukni         ###   ########.fr       */
+/*   Updated: 2020/06/19 18:33:38 by mazoukni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 **  Troisieme cour refrait 
 **  Variable en C
 **  Variables de type int (integer)
+**  chaine de charactere qu prondre le char 5 + caractere special
+**  ms bas no , tout a fait dans le memoire il s'agit une pointure
+**  et le type char il prent deux octets alors , on a 2 octects * 1 char + 
+**  special charactere
 
 */
 
@@ -25,8 +29,10 @@ int
     main(int argc, char *argv[])
 {
     int toto;
-    toto = 12;
-    printf ("Qutient    : %d\n", toto / 5);
-    printf ("Rest       : %d", toto % 5);
+    char* titi;
+    titi = "5";
+    toto = 5;
+    printf ("L'entier           : %d\n", toto);
+    printf ("Chaine de char     : %s", titi);
     return EXIT_SUCCESS;
 }
