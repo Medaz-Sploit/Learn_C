8378379b33781ac99f1a81c66f499824e8ff77cc
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2020-06-19 04:27:19 +0100

    Learning

--- troisieme.c
+++ troisieme.c
@@ -6,7 +6,7 @@
 /*   By: mazoukni <mazoukni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 01:50:26 by mazoukni          #+#    #+#             */
-/*   Updated: 2020/06/19 03:46:46 by mazoukni         ###   ########.fr       */
+/*   Updated: 2020/06/19 04:26:41 by mazoukni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ int
     main(int argc, char *argv[])
 {
     int toto;
-    toto = 5;
+    toto = 12;
+    printf ("Qutient    : %d\n", toto / 5);
+    printf ("Rest       : %d", toto % 5);
     return EXIT_SUCCESS;
 }
