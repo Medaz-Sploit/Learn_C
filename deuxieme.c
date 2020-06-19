ed8858fe0e6b98ebc1c76687c0020a6e2ea6d5ad
macbook pro <macbookprp@MacBook-Pro-de-macbook.local>
2020-06-19 03:24:41 +0100

    Learning

---
+++ deuxieme.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   deuxieme.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mazoukni <mazoukni@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/18 01:50:26 by mazoukni          #+#    #+#             */
+/*   Updated: 2020/06/19 03:19:52 by mazoukni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int 
+    main(int argc, char *argv[])
+{
+    char* a;
+    a = "C'est mon deuxieme program en C.\n";
+    a = "Le Soleil et Brille!\n";
+    printf (a);
+    return EXIT_SUCCESS;
+}
