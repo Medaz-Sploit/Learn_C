e6e9d2e6d9a9fd5a89ec8da4850ddfd16dbd85a5
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2020-06-19 03:50:21 +0100

    Learning

--- troisieme.c
+++ troisieme.c
@@ -1,14 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   deuxieme.c                                         :+:      :+:    :+:   */
+/*   troisieme.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mazoukni <mazoukni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 01:50:26 by mazoukni          #+#    #+#             */
-/*   Updated: 2020/06/19 03:40:05 by mazoukni         ###   ########.fr       */
+/*   Updated: 2020/06/19 03:46:46 by mazoukni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+
+**  Troisieme cour refrait 
+**  Variable en C
+**  Variables de type int (integer)
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,9 +24,7 @@
 int 
     main(int argc, char *argv[])
 {
-    char* a;
-    a = "C'est mon deuxieme program en C.\n";
-    a = "Le Soleil et Brille!\n";
-    printf (a);
+    int toto;
+    toto = 5;
     return EXIT_SUCCESS;
 }
